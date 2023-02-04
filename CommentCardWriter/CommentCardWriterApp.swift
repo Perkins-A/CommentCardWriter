5d4208d358a36ce8d996099c7f7268cfeeffc7ce
Alex Perkins <perkins.a@etoncollege.org.uk>
2023-02-04 10:50:31 +0000

    Initial Commit

---
+++ CommentCardWriter/CommentCardWriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardWriterApp.swift
+//  CommentCardWriter
+//
+//  Created by Perkins, Alexander (ABH) on 04/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardWriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
